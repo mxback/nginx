dc9a7fd3f1afd29bc2fcac0ff6a1f4c7a03dada2
test <65641723+mxback@users.noreply.github.com>
2020-06-05 14:11:37 +0800

    Add files via upload

---
+++ script/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#name: leo
+#create k8s-nginx
+echo "deploying..... SpaceName:${SpaceName},ContainerKey:${ContainerKey},ContainerValue:${ContainerValue},ImageName:${ImageName},Image:${Image},Name:${Name},ContainerPort:${ContainerPort},ContainerPortName:${ContainerPortName},PodPort:${PodPort}"
+rm -f deploy.yml
+cp -a ${WORKSPACE}/${JOB_NAME}/yaml/deploy.yml
+echo "copy success"
+sed -i "s,{{SpaceName}},${SpaceName},g" deploy.yaml
+sed -i "s,{{ContainerKey}},${ContainerKey},g" deploy.yaml
+sed -i "s,{{ContainerValue}},${ContainerValue},g" deploy.yaml
+sed -i "s,{{ImageName}},${ImageName},g" deploy.yaml
+sed -i "s,{{Image}},${Image},g" deploy.yaml
+sed -i "s,{{ContainerPort}},${ContainerPortName},g" deploy.yaml
+sed -i "s,{{ContainerPortName}},${ContainerPortName},g" deploy.yaml
+sed -i "s,{{PodPort}},${PodPort},g" deploy.yaml
+sed -i "s,{{Name}},${Name},g" deploy.yaml
+echo "read success"
+kubectl apply -f web.yaml
+echo "apply ok"
