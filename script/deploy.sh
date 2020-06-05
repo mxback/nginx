95c7dcef4fdd63758ace8d37ad90fc04bf044a65
test <65641723+mxback@users.noreply.github.com>
2020-06-05 14:29:19 +0800

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,15 +5,15 @@ echo "deploying..... SpaceName:${SpaceName},ContainerKey:${ContainerKey},Contain
 rm -f deploy.yml
 cp -a ${WORKSPACE}/yaml/deploy.yml .
 echo "copy success"
-sed -i "s,{{SpaceName}},${SpaceName},g" deploy.yaml
-sed -i "s,{{ContainerKey}},${ContainerKey},g" deploy.yaml
-sed -i "s,{{ContainerValue}},${ContainerValue},g" deploy.yaml
-sed -i "s,{{ImageName}},${ImageName},g" deploy.yaml
-sed -i "s,{{Image}},${Image},g" deploy.yaml
-sed -i "s,{{ContainerPort}},${ContainerPortName},g" deploy.yaml
-sed -i "s,{{ContainerPortName}},${ContainerPortName},g" deploy.yaml
-sed -i "s,{{PodPort}},${PodPort},g" deploy.yaml
-sed -i "s,{{Name}},${Name},g" deploy.yaml
+sed -i "s,{{SpaceName}},${SpaceName},g" ./deploy.yaml
+sed -i "s,{{ContainerKey}},${ContainerKey},g" ./deploy.yaml
+sed -i "s,{{ContainerValue}},${ContainerValue},g" ./deploy.yaml
+sed -i "s,{{ImageName}},${ImageName},g" ./deploy.yaml
+sed -i "s,{{Image}},${Image},g" ./deploy.yaml
+sed -i "s,{{ContainerPort}},${ContainerPortName},g" ./deploy.yaml
+sed -i "s,{{ContainerPortName}},${ContainerPortName},g" ./deploy.yaml
+sed -i "s,{{PodPort}},${PodPort},g" ./deploy.yaml
+sed -i "s,{{Name}},${Name},g" ./deploy.yaml
 echo "read success"
 kubectl apply -f deploy.yaml
 echo "apply ok"
